1887719a1e1b7dfd1eaa9e762a6fd6398f519a5d
morrillo <morrillo@umich.edu>
2020-06-22 11:18:24 +0000

    First commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,19 +1,18 @@
 {
-    "name": "Sale Multi Confirm",
+    "name": "Crea remito desde factura",
     'version': '13.0.1.3.0',
-    'category': 'Sale',
+    'category': 'Accounting',
     'sequence': 14,
     'license': 'AGPL-3',
     'summary': '',
     'depends': [
         'account',
-        'sale',
+        'stock',
     ],
     'external_dependencies': {
     },
     'data': [
-        'views/sale_view.xml',
-        'wizard/wizard_view.xml'
+        'views/account_view.xml',
     ],
     'demo': [
     ],
--- models/models.py
+++ models/models.py
@@ -4,15 +4,46 @@ from odoo import api, fields, models, tools, _
 from odoo.exceptions import UserError,ValidationError
 from odoo.tools import float_is_zero, pycompat
 from odoo.addons import decimal_precision as dp
-from datetime import date
+from datetime import date,datetime
 import os
 import base64
 from collections import defaultdict
 
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
+class AccountMove(models.Model):
+    _inherit = 'account.move'
 
-    journal_id = fields.Many2one('account.journal')
+    def create_picking(self):
+        if not self.picking_id and self.type == 'out_invoice':
+            picking_type_id = self.env['stock.picking.type'].search([('company_id','=',self.company_id.id),('code','=','outgoing')],limit=1)
+            if not picking_type_id:
+                raise ValidationError('No tiene configurada la entrega')
+            location_dest_id = self.env['stock.location'].search([('usage','=','customer')],limit=1)
+            if not location_dest_id:
+                raise ValidationError('No esta configurada la ubicacion del cliente')
+            vals_picking = {
+                    'picking_type_id': picking_type_id.id,
+                    'scheduled_date': datetime.now(),
+                    'origin': self.name,
+                    'partner_id': self.partner_id.id,
+                    'location_id': picking_type_id.default_location_src_id.id,
+                    'location_dest_id': location_dest_id.id,
+                    }
+            picking_id = self.env['stock.picking'].create(vals_picking)
+            self.picking_id = picking_id.id
+            for line in self.invoice_line_ids:
+                vals_move = {
+                        'picking_id': picking_id.id,
+                        'product_id': line.product_id.id,
+                        'name': line.name,
+                        'product_uom_qty': line.quantity,
+                        'product_uom': line.product_uom_id.id,
+                        'location_id': picking_type_id.default_location_src_id.id,
+                        'company_id': self.company_id.id,
+                        'location_dest_id': location_dest_id.id,
+                        }
+                move_id = self.env['stock.move'].create(vals_move)
+
+    picking_id = fields.Many2one('stock.picking',string='Remito')
 
 
 #class PurchaseOrder(models.Model):
